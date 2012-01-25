2defbd3dfd5d9d54e5fc5033855666829b64f3d1
mayooresan <mayu3g@gmail.com>
2012-01-25 22:30:28 +0530

    Management Panel UI design started

---
+++ Source/Rainbow Cabs/Management Panel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Rainbow_Cabs
+{
+    public partial class Management_Panel : Form
+    {
+        public Management_Panel()
+        {
+            InitializeComponent();
+        }
+    }
+}
